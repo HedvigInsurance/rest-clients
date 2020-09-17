94886d584c3de6269e092f2e6fd606979ca9f083
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2020-09-17 09:27:08 +0200

    initial commit

---
+++ src/main/kotlin/com/hedvig/restclients/RestClientsApplication.kt
@@ -0,0 +1,11 @@
+package com.hedvig.restclients
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestClientsApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestClientsApplication>(*args)
+}
